ea70573ee53cdcdba604ab944bec6a1b86689f8a
shubham-efeed <shubham@efeed.in>
2023-01-03 09:31:12 +0530

    demo

---
+++ app/src/main/java/com/example/firebasedemo/MainActivity.kt
@@ -0,0 +1,161 @@
+package com.example.firebasedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.firebasedemo.databinding.ActivityMainBinding
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private val personCollectionRef = Firebase.firestore.collection("persons")
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnUploadData.setOnClickListener {
+            val person = getOldPerson()
+            savePerson(person)
+        }
+
+        binding.btnUpdatePerson.setOnClickListener {
+            val oldPerson = getOldPerson()
+            val newPersonMap = getNewPersonMap()
+            updatePerson(oldPerson, newPersonMap)
+        }
+
+        binding.btnDeletePerson.setOnClickListener {
+            val oldPerson = getOldPerson()
+            deletePerson(oldPerson)
+        }
+
+        binding.btnRetrieveData.setOnClickListener {
+            retrievePersons()
+        }
+    }
+
+    private fun retrievePersons() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val querySnapshot = personCollectionRef
+                    .orderBy("age")
+                    .get()
+                    .await()
+                val sb = StringBuilder()
+                for (document in querySnapshot.documents) {
+                    val person = document.toObject<Person>()
+                    sb.append("$person\n")
+                }
+                withContext(Dispatchers.Main) {
+                    binding.tvPersons.text = sb.toString()
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+    }
+
+    private fun getOldPerson(): Person {
+        val firstName = binding.etFirstName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val age = binding.etAge.text.toString().toInt()
+        return Person(firstName, lastName, age)
+    }
+
+    private fun getNewPersonMap(): Person {
+        val firstName = binding.etNewFirstName.text.toString()
+        val lastName = binding.etNewLastName.text.toString()
+        val age = binding.etNewAge.text.toString()
+        return Person(firstName, lastName, age.toInt())
+    }
+
+
+    private fun deletePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        val personQuery = personCollectionRef
+            .whereEqualTo("firstName", person.firstName)
+            .whereEqualTo("lastName", person.lastName)
+            .whereEqualTo("age", person.age)
+            .get()
+            .await()
+        if (personQuery.documents.isNotEmpty()) {
+            for (document in personQuery) {
+                try {
+                    personCollectionRef.document(document.id).delete().await()
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        } else {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "No persons matched the query.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun updatePerson(person: Person, newPerson: Person) =
+        CoroutineScope(Dispatchers.IO).launch {
+            val personQuery = personCollectionRef
+                .whereEqualTo("firstName", person.firstName)
+                .whereEqualTo("lastName", person.lastName)
+                .whereEqualTo("age", person.age)
+                .get()
+                .await()
+            if (personQuery.documents.isNotEmpty()) {
+                for (document in personQuery) {
+                    try {
+                        personCollectionRef.document(document.id).set(
+                            newPerson,
+                            SetOptions.merge()
+                        ).await()
+                    } catch (e: Exception) {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "No persons matched the query.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
+
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Successfully saved data.", Toast.LENGTH_LONG)
+                    .show()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/firebasedemo/Person.kt
@@ -0,0 +1,7 @@
+package com.example.firebasedemo
+
+data class Person(
+    var firstName: String = "",
+    var lastName: String = "",
+    var age: Int = -1
+)
